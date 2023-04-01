0f02328156e5a88dd342a28840c32faabf02e791
shizuku3158 <ksgnshizuku@gmail.com>
2023-04-01 21:48:31 +0900

    script update 1.2.11
    added google sheet link

--- main.js
+++ main.js
@@ -79,7 +79,7 @@ var video_idx = {
 	date : 1
 };
 
-var version = "1.2.10";
+var version = "1.2.11";
 
 var key_hash = "3f01e53f1bcee58f6fb472b5d2cf8e00ce673b13599791d8d2d4ddcde3defbbb4e0ab7bc704538080d704d87d79d0410";
 

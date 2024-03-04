4e8741cf8f5ba4994d5c0434dff4d2567d3e6925
Anson <84370085+szk31@users.noreply.github.com>
2024-03-05 00:48:38 +0900

    script update 1.6.0d
    fixed mem-count table header not being visible

--- main.js
+++ main.js
@@ -91,7 +91,7 @@ var video_idx = {
 
 var video, entry;
 
-var version = "1.6.0c";
+var version = "1.6.0d";
 
 var key_hash = [
 	"473c05c1ae8349a187d233a02c514ac73fe08ff4418429806a49f7b2fe4ba0b7a36ba95df1d58b8e84a602258af69194", //thereIsNoPassword
